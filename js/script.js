e6fa9a54ff078d4b8663a2fb4e73a5debb5c8c62
hellen <jameshellen2241@gmail.com>
2024-03-20 06:59:28 +0300

    added some changes to my project

--- js/script.js
+++ js/script.js
@@ -4,40 +4,83 @@ const htmlCss = [
     A: { choice: "It's a programing language", correct: false },
     B: { choice: "It's a human language", correct: false },
     C: { choice: "It's a markup language", correct: true },
-    D: { choice: "None of the above", correct: false }
+    D: { choice: "None of the above", correct: false },
   },
   {
     question: "What is css?",
     A: { choice: "None of the above", correct: false },
     B: { choice: "It's the coloring of colors", correct: false },
     C: { choice: "It's making web look smart", correct: false },
-    D: { choice: "It's a cascading stylesheet", correct: true }
+    D: { choice: "It's a cascading stylesheet", correct: true },
   },
   {
     question: "What is HTML tags?",
-    A: { choice: "It's ", correct: true },
+    A: { choice: "It's a markup language for creating web pages", correct: true },
     B: { choice: "It's a closing tag", correct: false },
     C: { choice: "The interrractivity", correct: false },
-    D: { choice: "The closing element", correct: false }
-  }
+    D: { choice: "The closing element", correct: false },
+  },
 ];
-let currentInBox = 0;
-let currentQuestion = htmlCss[0];
-console.log(currentQuestion)
-const question = document.createElement("div");
-question.innerHTML = `
-<div class=questions-container>
-    <p>multiplechoices</p>
-    <p class="result"></p>
-    </div> 
-`;
-// question.append(question);
+
+let questions = document.querySelector(".quizes");
+
+const quizButton = document.querySelector(".btn");
+
+let currentIndex = 0;
+const renderQuestion = () => {
+  let currentQuestion = htmlCss[currentIndex];
+  const question = document.createElement("div");
+
+  question.innerHTML = `
+  <div>
+      <p>${currentQuestion.question}</p>
+      <p><input type="radio" name="quiz"> ${currentQuestion.A.choice}</p>
+     <p> <input type="radio" name="quiz"> ${currentQuestion.B.choice}</p>
+     <p> <input type="radio" name="quiz"> ${currentQuestion.C.choice}</p>
+     <p> <input type="radio" name="quiz"> ${currentQuestion.D.choice}</p>
+      </div> 
+  `;
+  questions.append(question);
+
+  currentIndex += 1;
+
+  console.log(currentQuestion);
+  console.log(currentIndex);
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  renderQuestion();
+});
+
+quizButton.addEventListener("click", () => {
+  questions.innerHTML = "";
+  if (currentIndex === (htmlCss.length-1)) {
+    quizButton.textContent = "Finish";
+  }
+  renderQuestion();
+
+});
+const startMinutes=10;
+let time=startMinutes * 60;
+
+const countdownEl=document.getElementById("countdown");
+setInterval(updateCountdown,1000);
+function udateCountdown(){
+const minutes=math.floor(time/60);
+let seconds=time % 60;
+seconds=seconds<10 ? "0" + seconds: seconds;
+countdownEl.innerHTML=`
+${minutes}:${seconds}`;
+time--;
+
+}
+console.log(countdownEl)
+
 // question.append(multiplechoices)
 
 // updateQuestio();
 // updateMultipleChoices();
 // console.log(question);
-
 
 // let button=document.querySelector(".btn");
 // button.addEventListener("click",()=>{
