89c0a59d710a69fef194e4bba471836934486b56
hellen <jameshellen2241@gmail.com>
2024-03-20 09:05:50 +0300

    created changes to my project

--- js/script.js
+++ js/script.js
@@ -60,13 +60,13 @@ quizButton.addEventListener("click", () => {
   renderQuestion();
 
 });
-const startMinutes=10;
+const startMinutes=5;
 let time=startMinutes * 60;
 
 const countdownEl=document.getElementById("countdown");
 setInterval(updateCountdown,1000);
-function udateCountdown(){
-const minutes=math.floor(time/60);
+function updateCountdown(){
+const minutes=Math.floor(time/60);
 let seconds=time % 60;
 seconds=seconds<10 ? "0" + seconds: seconds;
 countdownEl.innerHTML=`
